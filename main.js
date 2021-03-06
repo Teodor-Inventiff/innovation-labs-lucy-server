b0dec5fba80942edb20f108d4bff3d3a4c8e3e62
Teodor Virghileanu <teo.inventiff@gmail.com>
2021-03-06 20:15:00 +0200

    blind update

--- main.js
+++ main.js
@@ -3,7 +3,12 @@ const { port } = require('./options.json'); // options about the server
 const app = require('express')(); // boilerplate
 const http = require('http').Server(app);
 const io = require('socket.io')(http, {
-  cors: false,
+  cors: {
+    origin: '*',
+    methods: ["GET", "POST", "PUT"],
+    credentials: false,
+    withCredentials: false,
+  }
 });
 
 app.get('/', (req, res) => { // http request redirects here
